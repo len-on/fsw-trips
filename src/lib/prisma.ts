e61619925fd9cd61709eda57c117efea316b874d
len-on <lenon.assalin@gmail.com>
2023-07-13 01:35:52 -0300

    criação do projeto

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalFOrPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma = 
+globalFOrPrisma.prisma ||
+new PrismaClient({
+    log: ["query"],
+})
+
+if (process.env.NODE_ENV !== "production") globalFOrPrisma.prisma
