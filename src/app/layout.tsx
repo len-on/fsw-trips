f31c74c81ec074348f7b85a7087390fd335fa35d
len-on <lenon.assalin@gmail.com>
2023-08-17 16:57:05 -0300

    style: footer style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,43 @@
-import { NextAuthProvider } from '@/providers/auth'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import Header from "@/components/Header"
-import Footer from '@/components/Footer'
-import ToastProvider from '@/providers/toast'
+import { NextAuthProvider } from "@/providers/auth";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import ToastProvider from "@/providers/toast";
 
-const poppins = Poppins({ subsets: ['latin'], weight: [
-  "400",
-  "500",
-  "600",
-  "700",
-  "800",
-  "900",
-]})
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
-  title: 'FSW Trips',
-  description: 'Sistema de Reserva de Viagens',
-}
+  title: "FSW Trips",
+  description: "Sistema de Reserva de Viagens",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
         <NextAuthProvider>
           <ToastProvider>
-            <Header />
-            
-              {children}
+            <div className="flex flex-col h-screen">
+              <div className="h-[94px]">
+                <Header />
+              </div>
 
-            <Footer />
+              <div className="flex-1">{children}</div>
+
+              <Footer />
+            </div>
           </ToastProvider>
         </NextAuthProvider>
       </body>
     </html>
-  )
+  );
 }
