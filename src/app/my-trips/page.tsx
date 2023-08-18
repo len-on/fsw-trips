d87fac81d18743fc6661849c14593ef8290c4c54
len-on <lenon.assalin@gmail.com>
2023-08-17 23:23:56 -0300

    style: adapt mytrips page to desktop

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -1,15 +1,15 @@
-"use client"
+"use client";
 
-import { Prisma} from '@prisma/client';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
-import UserReservationItem from './components/UserReservationItem';
-import Button from '@/components/Button';
-import Link from 'next/link';
+import React, { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { Prisma } from "@prisma/client";
+import Link from "next/link";
+
+import UserReservationItem from "./components/UserReservationItem";
+import Button from "@/components/Button";
 
 const MyTrips = () => {
-
   const [reservations, setReservations] = useState<
     Prisma.TripReservationGetPayload<{
       include: { trip: true };
@@ -21,11 +21,12 @@ const MyTrips = () => {
   const router = useRouter();
 
   const fetchReservations = async () => {
-    const response = await fetch(`/api/user/${(data?.user as any)?.id}/reservations`)
+    const response = await fetch(`/api/user/${(data?.user as any)?.id}/reservations`);
+
     const json = await response.json();
 
     setReservations(json);
-  }
+  };
 
   useEffect(() => {
     if (status === "unauthenticated") {
@@ -37,24 +38,24 @@ const MyTrips = () => {
 
   return (
     <div className="container mx-auto p-5">
-      <h1 className="font-semibold text-primaryDarker text-xl">Minhas Viagens</h1>
+      <h1 className="font-semibold text-primaryDarker text-xl lg:mb-5">Minhas Viagens</h1>
       {reservations.length > 0 ? (
-        reservations?.map((reservation) => (
-          <UserReservationItem fetchReservations={fetchReservations} key={reservation.id} reservation={reservation}/>
-         ))
-        ) : (
-          <div className='flex flex-col'>
-            <p className="mt-2 font-medium text-primaryDarker">Você não tem nenhuma reserva!</p> 
+        <div className="flex flex-col lg:grid lg:grid-cols-3 lg:gap-14">
+          {reservations?.map((reservation) => (
+            <UserReservationItem fetchReservations={fetchReservations} key={reservation.id} reservation={reservation} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col lg:max-w-[500px]">
+          <p className="mt-2 font-medium text-primaryDarker">Você ainda não tem nenhuma reserva! =(</p>
 
-            <Link href="/">
-              <Button className="w-full mt-2">Fazer reserva</Button>
-            </Link>
-          </div>
-        )
-      }
+          <Link href="/">
+            <Button className="w-full mt-2 lg:mt-5">Fazer reserva</Button>
+          </Link>
+        </div>
+      )}
     </div>
-  ) 
-  
-}
- 
+  );
+};
+
 export default MyTrips;
